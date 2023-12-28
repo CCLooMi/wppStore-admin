46134c89677642409f4d331d6db73f32dba6b0f6
seemie <chenios@foxmail.com>
2023-12-28 14:40:38 +0800

    add login animate

--- ctrls/loginCtrl.js
+++ ctrls/loginCtrl.js
@@ -3,19 +3,32 @@
  */
 (function (app) {
     app.controller('loginCtrl', ['$scope', '$modal', '$state', 'S_user', function (scope, $modal, $state, S_user) {
+        scope.loading=false;
+        scope.fadeOut=false;
         scope.doLogin = function (lo) {
+            scope.loading=true;
             S_user.login(lo)
                 .then(function (u) {
                     if(u){
-                        $modal.toastAlert('Login successful.')
-                        .onDestroy(()=>$state.go('main'));
                         delete scope.lo.password;
+                        scope.loading=false;
+                        scope.fadeOut=true;
+                        setTimeout(()=>{
+                            scope.fadeOut=false;
+                            $state.go('main');
+                        },1000);
                         return;
                     }
                     $modal.alert('Login failed!','e');
+                    scope.loading=false;
                     return true;
                 },function(e){
-                    $modal.toastAlert(e.message||e,'e');
+                    if(e.message||typeof e == 'string'){
+                        $modal.toastAlert(e.message||e,'e');
+                    }else{
+                        $modal.toastAlertDetail('Request error!','Service Unavailable!','e');
+                    }
+                    scope.loading=false;
                     return true;
                 });
         }
