e9f202208fd1c86362c90dd63d7f338ee72819e9
Seemie <chenios@foxmail.com>
2024-04-22 10:54:04 +0800

    update

--- servs/storyServ.js
+++ servs/storyServ.js
@@ -2,10 +2,11 @@
  * Created by Guest on 2024/4/12 16:52:56.
  */
 (function (app) {
-    app.factory('S_story', ['$idb', '$modal', '$http', '$marked', function ($idb, $modal, $http, $marked) {
+    app.factory('S_story', ['$idb', '$modal', '$http', '$less', function ($idb, $modal, $http, $less) {
         function getDB() {
             return $idb.get('wpp-store-admin');
         }
+        let lastType = 'a';
         return {
             byPage: function (pg) {
                 if (app.useMysql) {
@@ -27,55 +28,111 @@
                 const db = getDB();
                 return new Promise(function (resolve, reject) {
                     const id = uuid();
-                    const newStory = { id: id };
+                    const newStory = { id: id, type: lastType };
                     let timeout;
                     const scope = { story: newStory };
-                    $modal.dialog('Select Story Template', app.getPaths('views/modal/selectStory.atom?'), scope)
-                        .width(768).height(480)
-                        .ok(function () {
-                            if (app.useMysql) {
-                                $http.post(`${app.serverUrl}/story/saveUpdate`)
-                                    .responseJson()
-                                    .jsonData(newStory)
-                                    .then(function (rsp) {
-                                        const data = rsp.response;
-                                        if (data[0]) {
-                                            $modal.alertDetail('Save story error', data[1], 'e');
-                                            resolve();
-                                            return;
-                                        }
-                                        resolve(newStory);
-                                        $modal.alert('Add story successd!', 's');
-                                        Atom.broadcastMsg('refreshStorys');
-                                    }, function (e) {
-                                        $modal.alertDetail('Save story error', Atom.formatError(e), 'e');
+                    function saveStory() {
+                        if (app.useMysql) {
+                            $http.post(`${app.serverUrl}/story/saveUpdate`)
+                                .responseJson()
+                                .jsonData(newStory)
+                                .then(function (rsp) {
+                                    const data = rsp.response;
+                                    if (data[0]) {
+                                        $modal.alertDetail('Save story error', data[1], 'e');
                                         resolve();
-                                    })
-                                return;
-                            }
-                            db.put('story', newStory)
-                                .then(function () {
+                                        return;
+                                    }
                                     resolve(newStory);
                                     $modal.alert('Add story successd!', 's');
                                     Atom.broadcastMsg('refreshStorys');
                                 }, function (e) {
                                     $modal.alertDetail('Save story error', Atom.formatError(e), 'e');
                                     resolve();
+                                })
+                            return;
+                        }
+                        db.put('story', newStory)
+                            .then(function () {
+                                resolve(newStory);
+                                $modal.alert('Add story successd!', 's');
+                                Atom.broadcastMsg('refreshStorys');
+                            }, function (e) {
+                                $modal.alertDetail('Save story error', Atom.formatError(e), 'e');
+                                resolve();
+                            });
+                    }
+                    $modal.dialog('Select Story Template', app.getPaths('views/modal/selectStory.atom?'), scope)
+                        .width(768).height(480)
+                        .ok(function () {
+                            lastType = newStory.type;
+                            const se = this.getBody().findOne(`input[value='${lastType}']+s-v`);
+                            se.setAttribute('ckeditor', '');
+                            const t = this.getBody().findOne('template');
+                            const df = t.content;
+                            df.append(se);
+
+                            const styleScript = df.querySelector('script');
+                            styleScript.remove();
+                            $less.renderToStyleElement(styleScript.innerHTML)
+                                .then(style => {
+                                    df.append(style);
+                                    editStory(df);
                                 });
                         })
-                        .cancel(function () {
-                            resolve();
-                        }).afterOpen(function (m) {
-                            const body = m.getBody();
-                            const prevew = body.findOne(".preview");
-                            scope.contentChange = function (v) {
-                                const pvSr = prevew.shadowRoot || prevew.attachShadow({ mode: 'open' });
-                                clearTimeout(timeout);
-                                timeout = setTimeout(() => {
-                                    $marked.renderTo(v, pvSr);
-                                }, 500);
-                            }
-                        });
+                        .cancel(resolve);
+                    function editStory(df) {
+                        const blobUrl = URL.createObjectURL(new Blob([''], { type: "text/html" }));
+                        function revokeUrl() {
+                            URL.revokeObjectURL(blobUrl);
+                        }
+                        const iframe = document.createElement('iframe');
+                        iframe.src = blobUrl;
+                        iframe.onload = function () {
+                            const iwindow = iframe.contentWindow;
+                            const doc = iframe.contentDocument || iwindow.document;
+                            doc.body.appendChild(df);
+                            const ckUrl = require.toUrl("ckeditor");
+                            $http
+                                .get(ckUrl)
+                                .responseBlob()
+                                .then(r => r.response)
+                                .then(blob => {
+                                    const comStyleUrl = Paths.get(Atom.swScope(), "styles/common.css");
+                                    const script = document.createElement('script');
+                                    script.src = URL.createObjectURL(blob);
+                                    doc.head.append(script);
+                                    script.onload = function () {
+                                        const sv = doc.body.querySelector('s-v');
+                                        const fTitle = sv.querySelector('.face>.title');
+                                        const fCBlock = sv.querySelector('.face>.footer>.content-block');
+                                        const bod = sv.querySelector('.face+.body');
+                                        const eles = [fTitle, fCBlock, bod];
+                                        const opts = { styles: [comStyleUrl],autoHideToolbar: true};
+                                        eles.forEach(function (e) {
+                                            if (e) {
+                                                console.log(['e', e]);
+                                                iwindow.InlineEditor.create(e, opts).then(editor => {
+                                                    console.log("ckeditor init ok");
+                                                });
+                                            }
+                                        });
+                                    };
+                                });
+                        }
+                        const style = document.createElement('style');
+                        style.setAttribute('type', 'text/less');
+                        style.innerHTML = `iframe{height: inherit;width: inherit;padding: 0;border: none !important;}`;
+                        const ddf = document.createDocumentFragment();
+                        ddf.append(iframe, style);
+                        $modal.dialog('Edit Story Template', ddf, scope)
+                            .width(768).height(480)
+                            .ok(function () {
+
+                            })
+                            .cancel(resolve)
+                            .onDestroy(revokeUrl);
+                    }
                 });
             },
             editStory: function (u) {
@@ -113,16 +170,6 @@
                     })
                     .cancel(function () {
                         cloneA2B(bakU, u);
-                    }).afterOpen(function (m) {
-                        const body = m.getBody();
-                        const prevew = body.findOne(".preview");
-                        scope.contentChange = function (v) {
-                            const pvSr = prevew.shadowRoot || prevew.attachShadow({ mode: 'open' });
-                            clearTimeout(timeout);
-                            timeout = setTimeout(() => {
-                                $marked.renderTo(v, pvSr);
-                            }, 500);
-                        }
                     });
             },
             delStory: function (u) {
